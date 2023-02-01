8e6cc7406ec1016091b7e0bade0b221f4222c3cf
Phil-Dabrowski <phil.dabrowski@kellogg.northwestern.edu>
2023-02-01 01:32:31 +0000

    Dynamic Challenge part 1

--- practice.rb
+++ practice.rb
@@ -61,3 +61,42 @@ puts "Brian's KelloggCoin balance is #{brians_balance}"
 puts "Evan's KelloggCoin balance is #{evans_balance}"
 puts "Anthony's KelloggCoin balance is #{anthonys_balance}"
 
+#THE CHALLENGE
+# The above solution gets the job done and there's no need to go any further.  However, if this solution is leaving you a bit dissatisfied, then you're starting to think like a programmer!  
+
+# Why might you be feeling dissatisfied - i.e. what are the drawbacks?
+
+# Well, this solution only works with these 4 specific users.  But what if we added someone new to the blockchain?  We would need to duplicate a lot of the code to accommodate other user balances.  Are there other ways to implement the solution that are not dependent on knowing who and how many users there are in the transactions?
+
+# See if you can challenge yourself!
+
+#users = ["ben","brian"]
+puts "Starting the dynamic process"
+users =[]
+
+for transaction in blockchain
+  #New User Logic Below
+  new_user_check = false
+  puts "my user count is #{users.size}"
+  for existing_user in users
+    if transaction["to_user"] !=  existing_user["name"] #|| transaction["from_user"] !=  existing_user["name"] 
+      new_user_check = true
+      #users.push({"name" => transaction["to_user"], "wallet" => 0})  #no need for hash infront variable within transaction
+    end
+  end
+  if new_user_check == true || users.size == 0
+    users.push({"name" => transaction["to_user"], "wallet" => 0})  #no need for hash infront variable within transaction
+  end 
+
+    #balance logic below 
+  for user in users
+    if transaction["from_user"] == user["name"]  # Must reference the "current user" not the array "users"
+      user["wallet"] = user["wallet"] - transaction["amount"]
+    end
+    if transaction["to_user"] == user["name"]  # Must reference the "current user" not the array "users"
+      user["wallet"] = user["wallet"] + transaction["amount"]
+    end
+  end
+end
+#puts users Update input statement to format outputs to match request on assignment. 
+puts users
